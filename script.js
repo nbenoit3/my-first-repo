cf60c9d0a257c7a5342c6ab405c42abafc955828
Nick Benoit <nbenoit3@gmail.com>
2018-09-20 20:41:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function marinaraSauce() {
+    var two = 2;
+    var oneFourth = "1/4";
+
+    console.log(`-Heat a medium-large saucepan over medium heat`);
+    console.log(`-Add ${two} tspns of oil and 5 garlic cloves, cook until golden, about " + two + " minutes`);
+    console.log(`-Add ${oneFourth} cup water, ${two} cans of crushed tomatos, and salt and season with black pepper to taste.`);
+    console.log(`-Cover the pot, bring to a boil, reduce the heat to a medium low, and simmer until the sauce is heated, about 10 minutes`);
+    console.log(`Stir in ${oneFourth} cup roughly chopped fresh basil, salt and pepper as needed`);
+}
+
+marinaraSauce();
